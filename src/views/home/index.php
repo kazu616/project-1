464ba8acc2e797a70634f7b63a0449d3b5b3e77b
kazu616 <101005058+kazu616@users.noreply.github.com>
2023-03-10 16:28:26 +0700

    login

--- src/views/home/index.php
+++ src/views/home/index.php
@@ -2,17 +2,17 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="../../css/output.css">
-  <title>Booksaw</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/output.css">
+    <title>Booksaw</title>
 </head>
 
 <body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
 </body>
 
 </html>
