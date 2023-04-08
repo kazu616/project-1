352983680e26474e46b2f53dd362fad80f87845a
dat563 <96569559+dat563@users.noreply.github.com>
2023-04-08 22:10:02 +0700

    a

--- src/views/layouts/footer.php
+++ src/views/layouts/footer.php
@@ -6,18 +6,44 @@
         <span>Saw</span>
       </h1>
       <div class="flex gap-x-20">
-        <?php for ($i = 1; $i < 5; $i++) { ?>
-          <div class="flex flex-col gap-y-3">
-            <h1 class="font-secondary text-[28px]">About us</h1>
-            <ul class="flex flex-col text-sm uppercase text-[#777777]">
-              <li>vision</li>
-              <li>articles</li>
-              <li>careers</li>
-              <li>service terms</li>
-              <li>donate</li>
-            </ul>
-          </div>
-        <?php } ?>
+        <div class="flex flex-col gap-y-3">
+          <h1 class="font-secondary text-[28px]">About us</h1>
+          <ul class="flex flex-col text-sm uppercase text-[#777777] gap-y-1">
+            <li>vision</li>
+            <li>articles</li>
+            <li>careers</li>
+            <li>service terms</li>
+            <li>donate</li>
+          </ul>
+        </div>
+        <div class="flex flex-col gap-y-3">
+          <h1 class="font-secondary text-[28px]">Discover</h1>
+          <ul class="flex flex-col text-sm uppercase text-[#777777] gap-y-1">
+            <li>home</li>
+            <li>books</li>
+            <li>authors</li>
+            <li>subjects</li>
+            <li>advanced search</li>
+          </ul>
+        </div>
+        <div class="flex flex-col gap-y-3">
+          <h1 class="font-secondary text-[28px]">My account</h1>
+          <ul class="flex flex-col text-sm uppercase text-[#777777] gap-y-1">
+            <li>sign in</li>
+            <li>view cart</li>
+            <li>my wishlist</li>
+            <li>trach my order</li>
+          </ul>
+        </div>
+        <div class="flex flex-col gap-y-3">
+          <h1 class="font-secondary text-[28px]">Help</h1>
+          <ul class="flex flex-col text-sm uppercase text-[#777777] gap-y-1">
+            <li>help center</li>
+            <li>report a problem</li>
+            <li>suggesting edits</li>
+            <li>contact us</li>
+          </ul>
+        </div>
       </div>
     </div>
   </div>
